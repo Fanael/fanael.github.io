54a5239143e14660ceb8fc3e052a17e7c5bae47f
Fanael Linithien <fanael4@gmail.com>
2022-01-30 08:00:58 +0100

    Update copyright years

--- greenspun/src/main/java/greenspun/generator/RenderConstants.java
+++ greenspun/src/main/java/greenspun/generator/RenderConstants.java
@@ -1,4 +1,4 @@
-// Copyright © 2021  Fanael Linithien
+// Copyright © 2021-2022  Fanael Linithien
 // SPDX-License-Identifier: AGPL-3.0-or-later OR CC-BY-SA-4.0
 package greenspun.generator;
 
@@ -8,6 +8,6 @@ final class RenderConstants {
     }
 
     static final String siteTitle = "Fanael's random ruminations";
-    static final String copyrightLine = "Copyright © 2019-2021 Fanael Linithien";
+    static final String copyrightLine = "Copyright © 2019-2022 Fanael Linithien";
     static final String feedFileName = "feed.xml";
 }
--- greenspun/src/test/resources/source/another.lisp
+++ greenspun/src/test/resources/source/another.lisp
@@ -1,4 +1,4 @@
-;; Copyright © 2021  Fanael Linithien
+;; Copyright © 2021-2022  Fanael Linithien
 ;; SPDX-License-Identifier: AGPL-3.0-or-later
 (defarticle
   :title "Another article"
