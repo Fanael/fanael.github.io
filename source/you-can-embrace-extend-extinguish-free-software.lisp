7a70074a77cd07b5345d6a8a129b48486da676fb
Fanael Linithien <fanael4@gmail.com>
2022-09-22 01:37:27 +0200

    New article: Yes, you can embrace, extend, extinguish free software

---
+++ source/you-can-embrace-extend-extinguish-free-software.lisp
@@ -0,0 +1,11 @@
+;; Copyright © 2022  Fanael Linithien
+;; SPDX-License-Identifier: AGPL-3.0-or-later OR CC-BY-SA-4.0
+(defarticle
+  :title "Yes, you can embrace, extend, extinguish free software"
+  :description "Proof that corporations have succeeded at embracing, extending and extinguishing free software"
+  :date (2022 09 22)
+  :topics nil
+  ;; Yes, the article body is just the name of the project in question. There's
+  ;; no need to elaborate, it's self-explanatory to anyone who knows their
+  ;; browser engine history.
+  (p "KHTML"))
