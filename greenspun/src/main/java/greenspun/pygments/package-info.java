ac4c3830807bc61ecf3534074c01a3d4655ecd58
Fanael Linithien <fanael4@gmail.com>
2024-04-24 14:35:12 +0200

    Update copyright years

--- greenspun/src/main/java/greenspun/pygments/package-info.java
+++ greenspun/src/main/java/greenspun/pygments/package-info.java
@@ -1,4 +1,4 @@
-// Copyright © 2021-2022  Fanael Linithien
+// Copyright © 2021-2024  Fanael Linithien
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 /**
--- greenspun/src/main/resources/pygments_server.py
+++ greenspun/src/main/resources/pygments_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright © 2019-2023  Fanael Linithien
+# Copyright © 2019-2024  Fanael Linithien
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # pylint: disable=missing-function-docstring, missing-class-docstring
 '''
